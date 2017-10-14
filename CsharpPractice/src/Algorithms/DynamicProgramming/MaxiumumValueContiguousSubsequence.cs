93fb1fbb9c42e4d6fa88c74c8bc9fc29ca36ca92
Jatin Sanghvi <jatin085@yahoo.com>
2017-10-14 18:29:36 +0530

    Fix folder structure and add main method

---
+++ CsharpPractice/src/Algorithms/DynamicProgramming/MaxiumumValueContiguousSubsequence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms
+{
+	internal static class MaxiumumValueContiguousSubsequence
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
---
+++ CsharpPractice/src/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+		List<Type> allClasses = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsClass).ToList();
+
+		List<string> namespaces =
+			allClasses.Select(c => c.Namespace).Where(n => !string.IsNullOrEmpty(n)).Distinct().OrderBy(n => n).ToList();
+
+		Console.WriteLine("List of namespaces:");
+
+		for (int i = 0; i < namespaces.Count; i++)
+		{
+			Console.WriteLine($"{i + 1}. {namespaces[i]}");
+		}
+
+		Console.Write("\nEnter namespace ID (0 to quit): ");
+
+		int namespaceId;
+		if (!int.TryParse(Console.ReadLine(), out namespaceId) || namespaceId <= 0 || namespaceId > namespaces.Count)
+		{
+			return;
+		}
+
+		Console.WriteLine();
+
+		List<string> classes =
+			allClasses.Where(c => c.Namespace == namespaces[namespaceId - 1]).Select(c => c.Name).OrderBy(n => n).ToList();
+
+		Console.WriteLine("List of classes:");
+
+		for (int i = 0; i < classes.Count; i++)
+		{
+			Console.WriteLine($"{i + 1}. {classes[i]}");
+		}
+
+		Console.Write("\nEnter class ID (0 to quit): ");
+
+		int classId;
+		if (!int.TryParse(Console.ReadLine(), out classId) || classId <= 0 || classId > classes.Count)
+		{
+			return;
+		}
+
+		Console.WriteLine();
+
+		Type.GetType(namespaces[namespaceId - 1] + '.' + classes[classId - 1]).GetMethod("Main")
+			.Invoke(null, new object[] { new string[0] });
+
+		Console.ReadKey();
+	}
+}
