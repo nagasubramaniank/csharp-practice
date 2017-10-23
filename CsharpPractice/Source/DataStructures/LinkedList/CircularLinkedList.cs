ab430e080cd1f5fc6d32757172cd35cc9751363e
Jatin Sanghvi <jatin085@yahoo.com>
2017-10-23 17:33:40 +0530

    Add 'insert into circular linked list' problem solution

---
+++ CsharpPractice/Source/DataStructures/LinkedList/CircularLinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace CsharpPractice.Source.DataStructures.LinkedList
+{
+    internal sealed class CircularLinkedList<TValue> where TValue : IComparable<TValue>
+    {
+        LinkedListNode<TValue> head;
+
+        public CircularLinkedList()
+        {
+            head = null;
+        }
+
+        public void Insert(TValue insertValue)
+        {
+            if (head == null)
+            {
+                this.head = new LinkedListNode<TValue> { value = insertValue };
+                this.head.next = this.head;
+                return;
+            }
+
+            if (insertValue.CompareTo(this.head.value) <= 0)
+            {
+                LinkedListNode<TValue> clonedHead = new LinkedListNode<TValue> { value = this.head.value, next = this.head.next };
+                this.head.value = insertValue;
+                this.head.next = clonedHead;
+                return;
+            }
+
+            LinkedListNode<TValue> predecessorNode = this.head;
+
+            for (; predecessorNode.next != this.head && predecessorNode.next.value.CompareTo(insertValue) < 0; predecessorNode = predecessorNode.next) ;
+
+            predecessorNode.next = new LinkedListNode<TValue> { value = insertValue, next = predecessorNode.next };
+        }
+
+        public override string ToString()
+        {
+            if (this.head == null)
+            {
+                return string.Empty;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            LinkedListNode<TValue> node = this.head;
+
+            do
+            {
+                stringBuilder.Append($"{node.value} ");
+                node = node.next;
+            }
+            while (node != this.head);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ CsharpPractice/Source/DataStructures/LinkedList/InsertIntoCircularLinkedList.cs
@@ -0,0 +1,27 @@
+﻿// Insert values into circular linked list.
+
+using System;
+
+namespace CsharpPractice.Source.DataStructures.LinkedList
+{
+    internal static class InsertIntoCircularLinkedList
+    {
+        public static void Main(string[] args)
+        {
+            PrintInsertIntoCircularLinkedList(-4, 0, -2, 2, -3, 1, -1, 3);
+        }
+
+        private static void PrintInsertIntoCircularLinkedList<TValue>(params TValue[] values) where TValue : IComparable<TValue>
+        {
+            CircularLinkedList<TValue> linkedList = new CircularLinkedList<TValue>();
+
+            Console.WriteLine($"Circular Linked List (before): {linkedList}");
+
+            foreach (TValue value in values)
+            {
+                linkedList.Insert(value);
+                Console.WriteLine($"Circular Linked List (after inserting {value, 2}): {linkedList}");
+            }
+        }
+    }
+}
