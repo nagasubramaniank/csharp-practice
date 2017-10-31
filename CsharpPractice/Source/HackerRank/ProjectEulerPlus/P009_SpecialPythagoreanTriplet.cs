4adb6d4e7931fb31bcea445c0cf9f0fb6c0c1028
Jatin Sanghvi <jatin085@yahoo.com>
2017-11-01 02:26:48 +0530

    Add solutions to HackerRank ProjectEuler+ problems

---
+++ CsharpPractice/Source/HackerRank/ProjectEulerPlus/P009_SpecialPythagoreanTriplet.cs
@@ -0,0 +1,53 @@
+﻿// Project Euler #9: Special Pythagorean triplet
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler009
+
+using System;
+
+namespace CsharpPractice.Source.HackerRank.ProjectEulerPlus
+{
+    internal static class P009_SpecialPythagoreanTriplet
+    {
+        public static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+
+            for (int testCase = 0; testCase < testCases; testCase++)
+            {
+                PrintSpecialPythagoreanTriplet(int.Parse(Console.ReadLine()));
+            }
+        }
+
+        private static void PrintSpecialPythagoreanTriplet(int sum)
+        {
+            // Sum of three sides should always be an even value.
+            if (sum % 2 != 0)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+
+            int maximumProduct = -1;
+
+            // Substituting value of c as N - a - b in equation a^2 + b^2 == c^2 gives value of b (side2) in terms of
+            // a (side1) and N. b = (N / 2) * (N - 2 * a) / (N - a).
+
+            for (int side1 = 3; side1 < sum / 3; side1++)
+            {
+                int side2Numerator = sum / 2 * (sum - 2 * side1);
+                int side2Denominator = sum - side1;
+
+                if (side2Numerator % side2Denominator != 0)
+                {
+                    continue;
+                }
+
+                int side2 = side2Numerator / side2Denominator;
+                int hypotenuse = sum - side1 - side2;
+
+                maximumProduct = Math.Max(maximumProduct, side1 * side2 * hypotenuse);
+            }
+
+            Console.WriteLine(maximumProduct);
+        }
+    }
+}
---
+++ CsharpPractice/Source/HackerRank/ProjectEulerPlus/P010_SummationOfPrimes.cs
@@ -0,0 +1,61 @@
+﻿// Project Euler #10: Summation of primes
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler010
+
+using System;
+
+namespace CsharpPractice.Source.HackerRank.ProjectEulerPlus
+{
+    internal static class P010_SummationOfPrimes
+    {
+        public static void Main(string[] args)
+        {
+            long[] sumsOfPrimes = GetSumOfPrimesUpto(1000000);
+
+            int testCases = int.Parse(Console.ReadLine());
+
+            for (int testCase = 0; testCase < testCases; testCase++)
+            {
+                PrintSummationOfPrimes(int.Parse(Console.ReadLine()), sumsOfPrimes);
+            }
+        }
+
+        private static long[] GetSumOfPrimesUpto(int limit)
+        {
+            bool[] isNotPrime = new bool[limit + 1];
+
+            isNotPrime[0] = isNotPrime[1] = true;
+
+            for (int number = 2; number * number <= limit; number++)
+            {
+                if (!isNotPrime[number])
+                {
+                    for (int multiple = 2 * number; multiple <= limit; multiple += number)
+                    {
+                        isNotPrime[multiple] = true;
+                    }
+                }
+            }
+
+            long[] sumsOfPrimes = new long[limit + 1];
+
+            long sumOfPrimes = 0;
+
+            for (int number = 0; number <= limit; number++)
+            {
+                if (!isNotPrime[number])
+                {
+                    sumOfPrimes = checked (sumOfPrimes + number);
+                }
+
+                sumsOfPrimes[number] = sumOfPrimes;
+            }
+
+            return sumsOfPrimes;
+        }
+
+        private static void PrintSummationOfPrimes(int number, long[] sumsOfPrimes)
+        {
+            Console.WriteLine(sumsOfPrimes[number]);
+        }
+    }
+}
